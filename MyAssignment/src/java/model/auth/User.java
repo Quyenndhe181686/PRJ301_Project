a568f134c2d9308a00d26a3c054d4a50eda7704e
Quyenndhe181686 <milo9a5@gmail.com>
2024-10-12 23:34:49 +0700

    model auth
    Made feature Role and User , made dbcontext class

---
+++ MyAssignment/src/java/model/auth/User.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.auth;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author milo9
+ */
+public class User {
+    
+    private String username;
+    private String displayName;
+    private ArrayList<Role> roles = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(ArrayList<Role> roles) {
+        this.roles = roles;
+    }
+    
+    
+}
